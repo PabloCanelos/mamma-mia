a5f5bd666fb76be87f1f708102719ed65ccf09df
Pablo Andres Canelos Canelos <pabloccanelos.contacto@gmail.com>
2025-02-21 10:33:32 -0300

    prueba

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,11 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
-      <h1 className='h1Header'>¡izzeria Mamma Mia!</h1>
+      <h1 className='h1Header'>¡Pizzeria Mamma Mia!</h1>
       <p>¡Tenemos las mejores pizzas que podrás encontrar!</p>
     </div>
   )
 }
 
 export default Header
+
